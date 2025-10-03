96edf2e2bc93d407d962ea0ef4373448873a2d0f
Goeunwoo <avouchre24@gmail.com>
2025-10-04 01:27:25 +0900

    feat: 로그인 페이지 UI 추가

--- battlemap-fe/src/Router.jsx
+++ battlemap-fe/src/Router.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Onboarding from "./pages/onboarding/Onboarding.jsx";
-
+import Login from "./pages/login/Login.jsx";
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Onboarding />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
