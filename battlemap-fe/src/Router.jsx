4b339445c5867308b5fb5578de57268424bfd242
Goeunwoo <avouchre24@gmail.com>
2025-10-04 23:59:16 +0900

    회원가입 페이지 UI

--- battlemap-fe/src/Router.jsx
+++ battlemap-fe/src/Router.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Onboarding from "./pages/onboarding/Onboarding.jsx";
 import Login from "./pages/login/Login.jsx";
+import Join from "./pages/join/Join.jsx";
+
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Onboarding />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
       </Routes>
     </BrowserRouter>
   );
