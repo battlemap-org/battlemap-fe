624fad8242e4261f48baa98bd562ca2b9e3a2b02
Goeunwoo <avouchre24@gmail.com>
2025-10-10 02:12:48 +0900

    도시 선택 페이지 완성

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Onboarding from "./pages/onboarding/Onboarding.jsx";
 import Login from "./pages/login/Login.jsx";
 import Join from "./pages/join/Join.jsx";
+import Filter from "./pages/filter/Filter.jsx";
 import Profile from "./pages/profile/Profile.jsx";
+import Whereistoday from "./pages/whereistoday/Whereistoday.jsx";
 
 export default function Router() {
   return (
@@ -12,6 +14,8 @@ export default function Router() {
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/filter" element={<Filter />} />
+        <Route path="/whereistoday" element={<Whereistoday />} />
       </Routes>
     </BrowserRouter>
   );
